bfda49a254a2c3fe39e1e6cf4b0de7592597b13c
echostorm17 <skydev359@outlook.com>
2024-09-05 22:17:06 -0400

    feat: pre data

--- main.py
+++ main.py
@@ -10,3 +10,18 @@ from sklearn.model_selection import train_test_split
 # Local path to the downloaded file
 file_path = "D:/code-test/1/riceClassification.csv"  # Adjust path as necessary
 data = pd.read_csv(file_path)
+# print(data.columns)
+# Assuming the dataset has features and a binary target variable 'type'
+X = data.drop('Class', axis=1)
+y = data['Class']
+print(X)
+print(y)
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+print(f'X_train: {X_train}')
+print(f'y_train: {y_train}')
+print(f'X_test: {X_test}')
+print(f'y_test: {y_test}')
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
